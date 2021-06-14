155cef083cb68aac7a4eb86c8430e5b972fd34f5
Guilherme de Prais <guilherme.prais@gmail.com>
2021-06-14 16:40:23 -0300

    req 01

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import logo from './trivia.png';
+import { Route, Switch } from 'react-router';
+import Login from './Pages/Login';
 import './App.css';
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          SUA VEZ
-        </p>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path="/" component={ Login } />
+    </Switch>
   );
 }
