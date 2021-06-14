13e6bdb45534b76c290d6d58de4344e1eead54d7
Paulo Vitor Borges <pvtrybe209@gmail.com>
2021-06-14 17:34:54 -0300

    requisito 3

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import logo from './trivia.png';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router';
+import Login from './Pages/Login';
 import './App.css';
+import Settings from './Pages/Settings';
 
-export default function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          SUA VEZ
-        </p>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/settings" component={ Settings } />
+        </Switch>
+      </div>
+    );
+  }
 }
+
+export default App;
