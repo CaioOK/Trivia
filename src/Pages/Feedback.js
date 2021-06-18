c6445e1bd7369d0839e83e76d090fb2d7efdecb2
Caio Oliveira <parafrenteeadiante@gmail.com>
2021-06-18 15:47:13 -0300

    iniciando req 13

--- src/Pages/Feedback.js
+++ src/Pages/Feedback.js
@@ -2,9 +2,24 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Feedback extends Component {
-  // constructor() {
-  //   super();
-  // }
+  constructor() {
+    super();
+
+    this.handleFeedbackMessage = this.handleFeedbackMessage.bind(this);
+  }
+
+  handleFeedbackMessage() {
+    const stateLS = JSON.parse(localStorage.getItem('state'));
+    const { assertions } = stateLS.player;
+    const tres = 3;
+    switch (assertions) {
+    case assertions < tres:
+      return 'Podia ser melhor...';
+    default:
+      return 'Mandou bem!';
+    }
+  }
+
   render() {
     return (
       <>
